503a3d96564e0deff17aa6f2f367edaaf27f9a81
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2022-12-18 14:33:06 -0300

    feat(introduction): adiciona mais 5 hosts e 1 switch
    Cenário:
    
        switches:
          - s1:
            - ports: | 1  | 2  | 3  | 4  | 5  |
              host:  | h1 | h2 | h3 | h4 | h5 |
            s2:
            - ports: | 1  | 2  | 3  | 4  | 5   |
              host:  | h6 | h7 | h8 | h9 | h10 |
    
        controllers:
          - c0:
            - ports:  | 1  | 2  |
              switch: | s1 | s2 |
    
                      +----------------c0----------------+
                      |                                  |
        +-------------s1------------+      +-------------s2------------+
        |      +------|------+      |      |      +------|------+      |
        |      |      |      |      |      |      |      |      |      |
        |      |      |      |      |      |      |      |      |      |
        h1 --- h2 --- h3 --- h4 --- h5     h6 --- h7 --- h8 --- h9 --- h10

--- 00-introduction/hello_topology.py
+++ 00-introduction/hello_topology.py
@@ -4,20 +4,24 @@ from mininet.topo import Topo
 class HelloTopology(Topo):
     def build(self):
         # Hosts
-        n_hosts = 5
+        n_hosts = 10
         hosts = []
         for n in range(1, n_hosts + 1):
             hosts.append(self.addHost('h' + str(n)))
 
         # Switches
-        n_switches = 1
+        n_switches = 2
         switches = []
         for n in range(1, n_switches + 1):
             switches.append(self.addSwitch('s' + str(n)))
 
-        # Likns to all hosts to s1
-        for host in hosts:
+        # Likns to hosts 1-5 to s1
+        for host in hosts[:5]:
             self.addLink(host, switches[0])
+
+        # Likns to hosts 6-10 to s2
+        for host in hosts[5:]:
+            self.addLink(host, switches[1])
 
 
 topos = {'HelloTopology': (lambda: HelloTopology())}
